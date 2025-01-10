747fc62fdd106f4ec2cfdebb98682a701ff9251c
ahmad <ahmad1zain10@gmail.com>
2025-01-10 23:51:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/xo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.xo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+
+public class MainActivity extends AppCompatActivity   {
+    CardView XCard, OCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        XCard = findViewById(R.id.XCard);
+        OCard = findViewById(R.id.OCard);
+        XCard.setOnClickListener(v -> navigateToGameActivity("X"));
+        OCard.setOnClickListener(v -> navigateToGameActivity("O"));
+    }
+
+    private void navigateToGameActivity(String firstPlayer){
+        Intent intent = new Intent(this, game_activity.class);
+        intent.putExtra("firstPlayer", firstPlayer);
+        startActivity(intent);
+    }
+    }
+
+
